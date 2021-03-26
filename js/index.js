04190e1da5fec8816928c8fca33c8e1126aee6b6
rodrigosta <rodrigo.contato1@outlook.com>
2021-03-25 21:27:03 -0300

    atualizacao

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// window.onscroll = () => {
+//     let nav = document.getElementById("navbar");
+//     if (document.body.scrollTop >= 280) { 
+//         nav.style.backgroundColor = "red";
+//     }
+//     else{
+//         nav.style.backgroundColor = "green";
+
+//     }
+// }
